a2a0f555da4d42f80070cacdb75f3d60b5abcec8
PARS303 <128808846+PARS303@users.noreply.github.com>
2023-04-15 22:02:33 +0300

    NextVersion

--- main.py
+++ main.py
@@ -1548,7 +1548,6 @@ async def delete_bot_messages(message: types.Message):
 				user.enginere_score += 1
 				massage = logic.request_in_massage(rows)
 				if rows != None:
-					print(massage)
 					bot_message = await message.answer(massage, reply_markup=kb5)
 					bot_messages.append(bot_message.message_id)
 					bot_messages.append(bot_message.message_id)
@@ -1565,7 +1564,6 @@ async def delete_bot_messages(message: types.Message):
 					rows = c.fetchone()
 					rows = rows[:-1]
 					rows2 = random.sample(rows[1:], len(rows[1:]))
-					print(list(rows))
 
 					k = ReplyKeyboardMarkup(resize_keyboard=True)
 					b1 = KeyboardButton(text=rows2[0])
@@ -1578,6 +1576,7 @@ async def delete_bot_messages(message: types.Message):
 					await message.answer(text=f"Перевидите слово {rows[0]}", reply_markup=k)
 
 					user.mode = '0_test_enginere'
+
 
 			if user.mode == 'guide':
 				c.execute(f'SELECT * FROM prof_gids where id ="{user.guide_score}"')
