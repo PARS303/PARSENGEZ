ddcaf086699f71d6e5dccec00f46c701a57ff46d
PARS303 <128808846+PARS303@users.noreply.github.com>
2023-04-15 21:50:24 +0300

    NextVersion

--- main.py
+++ main.py
@@ -2648,12 +2648,9 @@ async def test(message: types.Message):
 					await message.answer(text='Не верно')
 
 			if user.mode =='test_sport':
-				print('test')
 				c.execute(f'SELECT * FROM test_prof_sport where id = {str(user.l)}')
 				rows = c.fetchone()
 				rows = rows[:-1]
-				print(rows[user.i])
-				print(message.text)
 				if message.text == rows[user.i]:
 					user.i+=1
 					await message.answer(text ='Верно')
@@ -2687,11 +2684,7 @@ async def test(message: types.Message):
 							text=f'Набирите слово {c.execute("SELECT * FROM prof_sport").fetchall()[user.i][2]}',
 							reply_markup=kb)
 						else:
-							await message.answer(text='Все примеры были решены')
-							await message.answer(text='Вы в главном меню', reply_markup=kb2)
-
 							user.i = 0
-							user.mode = None
 				else:
 					await message.answer(text='Не верно')
 
