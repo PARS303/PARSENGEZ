f707161a0faac102eb32f41302dd2a62a9c865f0
PARS303 <128808846+PARS303@users.noreply.github.com>
2023-04-16 22:12:55 +0300

    NextVersion

--- main.py
+++ main.py
@@ -1866,8 +1866,24 @@ async def test(message: types.Message):
 					await message.answer(text='Не угадали')
 				if user.i == len(rows):
 					user.i = 0
-					await message.answer(text='Вы в главном меню', reply_markup=kb2)
-					user.mode = None
+					user.l = random.randint(0,
+											(len(c.execute(f'SELECT * FROM test_myself_basic_communication').fetchall()) - 1))
+
+					c.execute(f'SELECT * FROM test_myself_basic_communication where id = {str(user.l)}')
+					rows = c.fetchone()
+					rows = rows[:-1]
+					rows2 = random.sample(rows, len(rows))
+
+					k = ReplyKeyboardMarkup(resize_keyboard=True)
+					b1 = KeyboardButton(text=rows2[1])
+					b2 = KeyboardButton(text=rows2[2])
+					b3 = KeyboardButton(text=rows2[3])
+					b4 = KeyboardButton(text=rows2[4])
+					b5 = KeyboardButton(text=rows2[0])
+					b_menu = KeyboardButton(text="В главное меню")
+					k.add(b1, b2, b3, b4, b5,b_menu)
+
+					await message.answer(text="Составьте верное предложение из данных слов", reply_markup=k)
 			if user.mode =='0_test_communication':
 				c.execute(f'SELECT * FROM test_myself_basic_communication_0 where id = {user.l}')
 				rows = c.fetchone()
@@ -1875,14 +1891,37 @@ async def test(message: types.Message):
 
 				if message.text == rows[1]:
 					await message.answer(text='Верно', reply_markup=kb2)
-					user.mode = None
+
+					if user.i != len(c.execute(f'SELECT * FROM test_myself_basic_communication_0').fetchall()):
+
+						user.l = random.randint(0, (
+								len(c.execute(f'SELECT * FROM test_myself_basic_communication_0').fetchall()) - 1))
+
+						c.execute(f'SELECT * FROM test_myself_basic_communication_0 where id = {user.i}')
+						rows = c.fetchone()
+						rows = rows[:-1]
+						rows2 = random.sample(rows[1:], len(rows[1:]))
+
+						k = ReplyKeyboardMarkup(resize_keyboard=True)
+						b1 = KeyboardButton(text=rows2[0])
+						b2 = KeyboardButton(text=rows2[1])
+						b3 = KeyboardButton(text=rows2[2])
+						b4 = KeyboardButton(text=rows2[3])
+						b5 = KeyboardButton(text='В главное меню')
+						k.add(b1, b2, b3, b4, b5)
+
+						await message.answer(text=f"Перевидите слово {rows[0]}", reply_markup=k)
+						user.i+=1
+					else:
+						await message.answer(text='Вы успешно прошли тест')
+						user.i = 0
 				else:
 					await message.answer(text='Не угадали')
 			if user.mode == 'test_communication_1':
 				kb = ReplyKeyboardMarkup(resize_keyboard=True)
 				b_menu = KeyboardButton(text="В главное меню")
 				kb.add(b_menu)
-				if message.text == c.execute("SELECT * FROM myself_basic_communication").fetchall()[user.i][1]:
+				if str(message.text).lower().replace(' ','') == str(c.execute("SELECT * FROM myself_basic_communication").fetchall()[user.i][1]).lower().replace(' ',''):
 					if user.i != (len(c.execute("SELECT * FROM myself_basic_communication").fetchall())):
 						await message.answer(text='Верно')
 						user.i+=1
@@ -1891,10 +1930,10 @@ async def test(message: types.Message):
 							text=f'Набирите слово {c.execute("SELECT * FROM myself_basic_communication").fetchall()[user.i][2]}',
 							reply_markup=kb)
 						else:
-							await message.answer(text='Все примеры были решены')
-							await message.answer(text='Вы в главном меню', reply_markup=kb2)
 							user.i = 0
-							user.mode = None
+							await message.answer(
+								text=f'Набирите слово {c.execute("SELECT * FROM myself_basic_communication").fetchall()[user.i][2]}',
+								reply_markup=kb)
 				else:
 					await message.answer(text='Не верно')
 
